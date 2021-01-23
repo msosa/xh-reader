4cb611913a7b784f7db1f976ac744fab09a8addd
msosa <m.sosa1127@gmail.com>
2021-01-23 13:10:36 -0500

    upgrade to angular 11

--- src/app/file-reader/file-reader.component.spec.ts
+++ src/app/file-reader/file-reader.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FileReaderComponent } from './file-reader.component';
 
@@ -6,7 +6,7 @@ describe('FileReaderComponent', () => {
   let component: FileReaderComponent;
   let fixture: ComponentFixture<FileReaderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FileReaderComponent ]
     })
